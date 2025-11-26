cf563247cde214bd71d1d7ec84f761434df97461
yanis <yanismzyene7@gmail.com>
2025-11-26 15:08:49 +0100

    commnentaire retirer

--- src/fr/uge/implement/Dungeon.java
+++ src/fr/uge/implement/Dungeon.java
@@ -20,25 +20,24 @@ public class Dungeon {
     private MapDungeon createFloor(String prefix) {
         MapDungeon floor = new MapDungeon();
 
-        // 3 salles ennemies
         floor.add(new Room(prefix + " Enemy Room A"));
         floor.add(new Room(prefix + " Enemy Room B"));
         floor.add(new Room(prefix + " Enemy Room C"));
 
-        // 1 marchand
+
         floor.add(new Room(prefix + " Merchant Room"));
 
-        // 1 guérisseur
+
         floor.add(new Room(prefix + " Healer Room"));
 
-        // 2 trésors
+
         floor.add(new Room(prefix + " Treasure Room 1"));
         floor.add(new Room(prefix + " Treasure Room 2"));
 
-        // 1 sortie
+ 
         floor.add(new Room(prefix + " Exit Door"));
 
-        // Quelques couloirs
+  
         floor.add(new Room(prefix + " Corridor 1"));
         floor.add(new Room(prefix + " Corridor 2"));
         floor.add(new Room(prefix + " Corridor 3"));
