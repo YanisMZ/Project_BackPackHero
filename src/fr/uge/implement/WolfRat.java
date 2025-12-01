ec3b5844be870eb955fd29602ee3f2e9c40ddebb
yanis <yanismzyene7@gmail.com>
2025-12-01 16:08:03 +0100

    combat

---
+++ src/fr/uge/implement/WolfRat.java
@@ -0,0 +1,24 @@
+package fr.uge.implement;
+
+public record WolfRat(int hp, int protection) implements Enemy {
+
+    public WolfRat() {
+        this(25, 0);
+    }
+
+    @Override
+    public boolean isAlive() { return hp > 0; }
+
+    @Override
+    public Enemy takeDamage(int dmg) {
+        int finalDmg = Math.max(0, dmg - protection);
+        return new WolfRat(hp - finalDmg, protection);
+    }
+
+    @Override
+    public Enemy defend() {
+        return new WolfRat(hp, protection + 2);
+    }
+
+   
+}
